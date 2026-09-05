1ec3b422abd31e35ea3bf9196bed0705af295f77
engrahmad <ahmadyakubuaya@gmail.com>
2026-09-05 20:42:56 +0100

    Updated Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
-
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
 import { StudentsModule } from './students/students.module.js';
